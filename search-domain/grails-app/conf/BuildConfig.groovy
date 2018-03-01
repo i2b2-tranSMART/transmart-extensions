0943bddb3a683bac7abe7875d5c97bea5889048e
Burt Beckwith <burt@burtbeckwith.com>
2018-03-01 13:51:15 -0500

    cleanup, version updates

--- search-domain/grails-app/conf/BuildConfig.groovy
+++ search-domain/grails-app/conf/BuildConfig.groovy
@@ -1,34 +1,26 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.work.dir = 'target'
 
-grails.project.dependency.resolver = "maven"
+grails.project.dependency.resolver = 'maven'
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-	legacyResolve false
-    repositories {
-         grailsCentral()
-        mavenCentral()
-        mavenLocal() // Note: use 'grails maven-install' to install required plugins locally
-        mavenRepo "https://repo.transmartfoundation.org/content/repositories/public/"
-    }
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
-        // runtime 'mysql:mysql-connector-java:5.1.13'
-    }
-    plugins {
-		build(":release:3.0.1",
-			":rest-client-builder:1.0.3") {
-		export = false}
-        compile ':biomart-domain:16.2-SNAPSHOT'
-        //// already included in biomart-domain
-        //compile ':transmart-java:16.2-SNAPSHOT'
-    }
+	inherits 'global'
+	log 'warn'
+	legacyResolve false
+
+	repositories {
+		mavenLocal() // Note: use 'grails maven-install' to install required plugins locally
+		grailsCentral()
+		mavenCentral()
+		mavenRepo 'https://repo.transmartfoundation.org/content/repositories/public/'
+	}
+
+	plugins {
+		compile ':biomart-domain:16.2'
+		//// already included in biomart-domain
+		//compile ':transmart-java:16.2-SNAPSHOT'
+
+		build ':release:3.1.0', ':rest-client-builder:2.1.0', {
+			export = false
+		}
+	}
 }
